ecc373890e84a0d47f2d50e439f573f54bbe4956
dashi <1165371095@qq.com>
2020-01-03 16:36:38 +0800

    新增方法

--- src/main/java/com/gt/ssm/controller/DepartmentController.java
+++ src/main/java/com/gt/ssm/controller/DepartmentController.java
@@ -1,5 +1,8 @@
 package com.gt.ssm.controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * @Auther: wb_cheng
  * @Date: 2020/1/3 16:30
@@ -7,4 +10,11 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class DepartmentController {
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        System.out.println("test add");
+        return "list";
+    }
+
 }
