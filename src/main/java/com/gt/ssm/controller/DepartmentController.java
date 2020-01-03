22e6524d764c8b104bcd5ad4317600b447a501da
linghuchong123321 <53311368+linghuchong123321@users.noreply.github.com>
2020-01-03 16:34:03 +0800

    uodate
    ydaote

--- src/main/java/com/gt/ssm/controller/DepartmentController.java
+++ src/main/java/com/gt/ssm/controller/DepartmentController.java
@@ -1,9 +1,10 @@
 package com.gt.ssm.controller;
-
+import org.springframework.stereotype.Controller;
 /**
  * @Auther: wb_cheng
  * @Date: 2020/1/3 16:30
  * @Description:
  */
+@Controller
 public class DepartmentController {
 }
